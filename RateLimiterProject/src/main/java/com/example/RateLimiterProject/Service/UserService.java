a46df2d45f9d4819b85ff3494cf38927d9f798ac
Rishit Roy <hellorishit.roy@gmail.com>
2026-06-19 01:04:27 +0530

    Adding Authentication into the Mini Project.

---
+++ RateLimiterProject/src/main/java/com/example/RateLimiterProject/Service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.RateLimiterProject.Service;
+
+import com.example.RateLimiterProject.Entity.User;
+import com.example.RateLimiterProject.Exceptions.ResourceNotFoundException;
+import com.example.RateLimiterProject.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+
+}
