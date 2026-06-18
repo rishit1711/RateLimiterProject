ed7a6d5c0c7f07d4ea3de39dd71e26a9a128df11
Rishit Roy <hellorishit.roy@gmail.com>
2026-06-18 20:55:25 +0530

    Initial commit

---
+++ RateLimiterProject/src/test/java/com/example/RateLimiterProject/RateLimiterProject/RateLimiterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RateLimiterProject.RateLimiterProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateLimiterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
