4fd761d2a01d1e59791d0a84ee818ecc695861d3
Rishit Roy <hellorishit.roy@gmail.com>
2026-06-18 21:50:42 +0530

    Checkk...

--- RateLimiterProject/src/main/java/com/example/RateLimiterProject/RateLimiterProjectApplication.java
+++ RateLimiterProject/src/main/java/com/example/RateLimiterProject/RateLimiterProjectApplication.java
@@ -1,4 +1,4 @@
-package com.example.RateLimiterProject.RateLimiterProject;
+package com.example.RateLimiterProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +8,7 @@ public class RateLimiterProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RateLimiterProjectApplication.class, args);
+
 	}
 
 }
