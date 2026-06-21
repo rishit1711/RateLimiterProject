e732f3920de4c0d0416660b651cee61692e13df1
Rishit Roy <hellorishit.roy@gmail.com>
2026-06-21 23:49:56 +0530

    Api Key Generation

--- RateLimiterProject/src/main/java/com/example/RateLimiterProject/Entity/ApiKey.java
+++ RateLimiterProject/src/main/java/com/example/RateLimiterProject/Entity/ApiKey.java
@@ -1,6 +1,7 @@
 package com.example.RateLimiterProject.Entity;
 
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,13 +10,14 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Entity
+@Builder
 public class ApiKey {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
     @Column(unique = true,nullable = false)
     private String key;
-    private boolean isActive;
+//    private boolean isActive;
     @Column(nullable = false)
     private Integer rateLimit;
     private LocalDateTime createdAt;
