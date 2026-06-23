c4d923f77279209aadfe21f6980cc403178ed625
Rishit Roy <hellorishit.roy@gmail.com>
2026-06-24 00:04:35 +0530

    Started with Redis Counter Setup

---
+++ RateLimiterProject/src/main/java/com/example/RateLimiterProject/Service/RateLimiterService.java
@@ -0,0 +1,34 @@
+package com.example.RateLimiterProject.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+
+@Service
+public class RateLimiterService {
+
+    private static final int LIMIT = 10;
+    private static final long WINDOW_SECONDS = 60;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    public boolean allowRequest(String apiKey) {
+
+        String redisKey = "rate_limit:" + apiKey;
+
+        Long count = redisTemplate.opsForValue().increment(redisKey);
+
+        // First request
+        if (count != null && count == 1) {
+            redisTemplate.expire(
+                    redisKey,
+                    Duration.ofSeconds(WINDOW_SECONDS)
+            );
+        }
+
+        return count != null && count <= LIMIT;
+    }
+}
