81c93d567f417847cc56a6863feeba3c5384d211
Rishit Roy <hellorishit.roy@gmail.com>
2026-06-24 00:19:56 +0530

    Started with Redis Counter Setup

--- RateLimiterProject/src/main/java/com/example/RateLimiterProject/Redis/RateLimitingFilter.java
+++ RateLimiterProject/src/main/java/com/example/RateLimiterProject/Redis/RateLimitingFilter.java
@@ -1,4 +1,41 @@
 package com.example.RateLimiterProject.Redis;
 
-public class RateLimitingFilter{
+import com.example.RateLimiterProject.Service.RateLimiterService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@RequiredArgsConstructor
+public class RateLimitingFilter extends OncePerRequestFilter {
+    private final RateLimiterService rateLimiterService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String ApiKey = request.getHeader("API-KEY");
+
+        //api key missing
+        if(ApiKey==null || ApiKey.isBlank()){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Api Key is Required");
+            return;
+
+        }
+
+        boolean allowed = rateLimiterService.allowRequest(ApiKey);
+        if (!allowed) {
+            response.setStatus(429);
+            response.getWriter().write("Rate limit exceeded");
+            return;
+        }
+
+        filterChain.doFilter(request,response);
+
+
+
+
+    }
 }
