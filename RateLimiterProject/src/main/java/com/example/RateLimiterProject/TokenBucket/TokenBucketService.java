1385ae9268e1f79b2fe385c0cd8f022ecd17af53
Rishit Roy <hellorishit.roy@gmail.com>
2026-06-26 01:21:42 +0530

    TokenBucketService Implementation.

--- RateLimiterProject/src/main/java/com/example/RateLimiterProject/TokenBucket/TokenBucketService.java
+++ RateLimiterProject/src/main/java/com/example/RateLimiterProject/TokenBucket/TokenBucketService.java
@@ -4,15 +4,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 @Service
 
 public class TokenBucketService {
+
+    private static  Long CAPACITY;
 
     @Autowired
     private RedisTemplate<String,Object> redisTemplate;
 
 
     public boolean AllowRequest(String apiKey){
+
+        String redisKey = "bucket :"+ apiKey;
+        Bucket bucket =
+                (Bucket)
+                        redisTemplate.opsForValue().get(redisKey);
+
+
+        if(bucket==null){
+
+            bucket = new Bucket();
+            bucket.setTokens(CAPACITY);
+            bucket.setLastRefillTime(Instant.now().getEpochSecond());
+
+        }
+
+
         return  false;
 
     }
